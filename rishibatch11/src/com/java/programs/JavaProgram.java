59d8c95a8238eaaacab0c498915dd3acc34e3378
rushi <rushi@Rishii>
2025-08-16 23:18:38 +0530

    First Java Program

---
+++ rishibatch11/src/com/java/programs/JavaProgram.java
@@ -0,0 +1,9 @@
+package com.java.programs;
+
+public class JavaProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java!");
+	}
+
+}
---
+++ rishibatch11/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module rishibatch11 {
+}
