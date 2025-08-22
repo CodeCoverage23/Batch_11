9d1c357099ed1723e5372a021bf791c1faa63954
swapn <swapn@LAPTOP-BJNVAM67.lan>
2025-08-22 09:15:53 +0530

    banking service app

---
+++ batch11/src/com/basic/LoopDoWhileDemo.java
@@ -0,0 +1,18 @@
+package com.basic;
+
+public class LoopDoWhileDemo {
+
+	public static void main(String[] args) {
+
+		int a = 1;
+
+		do {
+			System.out.println(a * 3);
+
+			a++;
+
+		} while (a <= 10);
+
+	}
+
+}
