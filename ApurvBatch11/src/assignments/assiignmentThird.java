a472fddb4e377c7f0e8c7f57ed1a7b69da85141c
Vaishnavi <vkakad97@gmail.com>
2025-10-11 09:47:16 +0530

    Intial commit

---
+++ ApurvBatch11/src/assignments/assiignmentThird.java
@@ -0,0 +1,16 @@
+package assignments;
+
+public class assiignmentThird {
+
+	public static void main(String[] args) { 
+		String StudentName ="apurv";
+		System.out.println(StudentName);
+		
+		
+		
+		
+	
+
+	}
+
+}
