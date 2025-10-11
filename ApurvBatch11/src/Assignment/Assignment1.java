45b595e5296aadefebe76342e441acfdf8871cae
Apurv <kakadapurv@gmail.com>
2025-10-11 09:41:28 +0530

    intial changes

---
+++ ApurvBatch11/src/Assignment/Assignment1.java
@@ -0,0 +1,40 @@
+package Assignment;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		//assignment 2
+		//1. declare java primitive data types
+	
+	byte b=10;
+	short s=10234;
+	int o= 1028789654;
+	long l =10266536475859L;
+	boolean z= true;
+	double d=10.3648;
+	float f=1.3575673775785F;
+	char c='r';
+	      
+	//2.
+	char e='a';
+	 //3
+	int x;
+	System.out.println(o);
+	
+	//4
+	int v= 50;
+	System.out.println(v);
+	 //5
+	
+	
+	
+	 
+	
+	
+	
+	
+	
+
+	}
+
+}
---
+++ ApurvBatch11/src/assignments/Assignment5.java
@@ -0,0 +1,20 @@
+package assignments;
+
+public class Assignment5 {
+	public static void main(String[] args) {
+		String number;
+		int a =23;
+		if(a>0)
+		{
+			System.out.println(a+ " Number is Positive ");
+		}
+		else 
+		{
+			System.out.println(a+ " Number is Negative ");
+		}
+		
+		
+		
+	}
+
+}
