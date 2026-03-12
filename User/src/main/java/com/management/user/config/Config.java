e767def206985797f6e606ab0f62be48eb20af04
CodeCoverage23 <codecoverage23@gmail.com>
2026-03-12 09:03:54 +0530

    web client implementaion

--- User/src/main/java/com/management/user/config/Config.java
+++ User/src/main/java/com/management/user/config/Config.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class Config {
@@ -13,5 +14,12 @@ public class Config {
     RestTemplate loadBalancedRestTemplate() {
         return new RestTemplate();
     }
+    
+    
+    @Bean
+    @LoadBalanced
+    WebClient.Builder webClient() {
+		return WebClient.builder();
+	}
 
 }
--- User/src/main/java/com/management/user/services/UserServiceImpl.java
+++ User/src/main/java/com/management/user/services/UserServiceImpl.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.management.user.Exception.ResourceNotFoundException;
 import com.management.user.entity.Hotel;
@@ -23,8 +23,16 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	UserRepository userRepository;
 
+	/*
+	 * // RestTemplate is used to make HTTP requests to other microservices. It is
+	 * injected using @Autowired annotation.
+	 * 
+	 * @Autowired 
+	 * RestTemplate restTemplate;
+	 */
+	
 	@Autowired
-	RestTemplate restTemplate;
+	WebClient.Builder webClient;
 
 	@Override
 	public User saveUser(User user) {
@@ -45,19 +53,43 @@ public class UserServiceImpl implements UserService {
 
 		User user = userRepository.findById(UserId).orElseThrow(
 				() -> new ResourceNotFoundException("user with gievn id is not found in server " + UserId));
-		// Request ratings as an array of Rating to avoid Jackson deserializing into LinkedHashMap
-		ResponseEntity<Rating[]> response = restTemplate.getForEntity("http://RATING-MANAGEMENT-SYSTEM/ratings/all/" + user.getId(), Rating[].class);
-		Rating[] ratingArray = response.getBody();
-		List<Rating> ratings = ratingArray == null ? new ArrayList<>() : Arrays.asList(ratingArray);
-         System.out.println("Ratings for user " + user.getId() + ": " + ratings);
+		// Request ratings as an array of Rating to avoid Jackson deserializing into
+		// LinkedHashMap
+		/*
+		 * ResponseEntity<Rating[]> response =
+		 * restTemplate.getForEntity("http://RATING-MANAGEMENT-SYSTEM/ratings/all/" +
+		 * user.getId(), Rating[].class); 
+		 * Rating[] ratingArray = response.getBody();
+		 * List<Rating> ratings = ratingArray == null ? new ArrayList<>() :
+		 * Arrays.asList(ratingArray); System.out.println("Ratings for user " +
+		 * user.getId() + ": " + ratings);
+		 */
 //		 List<Rating> ratings = Arrays.stream(ratingOfUser).toList();
-	 	 
-		 List<Rating> collect = ratings.stream().map(rating -> {
-			ResponseEntity<Hotel> forEntity = restTemplate
-					.getForEntity("http://HOTEL-MANAGEMENT-SYSTEM/hotel/get/" + rating.getHotelId(), Hotel.class);
 
-			Hotel hotel = forEntity.getBody();
-			rating.setHotel(hotel);
+		/*
+		 * List<Rating> collect = ratings.stream().map(rating -> { ResponseEntity<Hotel>
+		 * forEntity = restTemplate
+		 * .getForEntity("http://HOTEL-MANAGEMENT-SYSTEM/hotel/get/" +
+		 * rating.getHotelId(), Hotel.class); // Hotel hotel = forEntity.getBody();
+		 */
+		// Using WebClient to fetch hotel details for each rating
+		Rating[] response = webClient.build().get()
+				.uri("http://RATING-MANAGEMENT-SYSTEM/ratings/all/" + user.getId())
+				.retrieve()
+				.bodyToMono(Rating[].class)
+				.block();
+		List<Rating> ratings = response == null ? new ArrayList<>() :
+			  Arrays.asList(response);
+		
+		List<Rating> collect = ratings.stream().map(rating -> {
+			Hotel forEntity = webClient.build().get()
+					.uri("http://HOTEL-MANAGEMENT-SYSTEM/hotel/get/" + rating.getHotelId())
+					.retrieve()
+					.bodyToMono(Hotel.class)
+					.block();
+		
+
+			rating.setHotel(forEntity);
 			return rating;
 
 		}).collect(Collectors.toList());
